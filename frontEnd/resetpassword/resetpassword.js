1c2868918acf05d41995ce31222e735b4064d3cd
root <root@ip-172-31-6-226.ap-south-1.compute.internal>
2023-11-29 05:05:15 +0000

    Initial commit

---
+++ frontEnd/resetpassword/resetpassword.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("formField");
+
+
+
+form.addEventListener('submit',newPassword);
+
+function newPassword(e) {
+    e.preventDefault();
+    const newpassword = e.target.password1.value;
+    console.log("new password changed is:",newpassword);
+    axios.post("http://65.0.39.128:4000/password/newpassword",{newpassword})
+    .then(() => {
+        console.log("new password changed");
+    }).catch(err => {
+        console.log(err);
+    })
+}
